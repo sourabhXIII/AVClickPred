31ad0fcc0efd1e1509ff8fcc24748fa1f6699260
Sourabh Maity <sourabh.maity@teradata.com>
2017-09-16 00:37:28 +0530

    some changes

--- AVclickPred.py
+++ AVclickPred.py
@@ -4,8 +4,6 @@ from data_exploration import DataExploration as DE
 # read data set
 raw_data = pandas.read_csv("./data/train.csv", nrows=10000, index_col=False)
 df = raw_data
-# have a glimpse of the data
-print(raw_data.head(5))
 
 # let's explore the data
 DE.explore_data(raw_data)
--- data_exploration.py
+++ data_exploration.py
@@ -3,12 +3,17 @@ class DataExploration:
 
     @staticmethod
     def explore_data(df):
-        # print the total number of rows
-        print("Total rows = %s" % (len(df)))
+        # have a glimpse of the data
+        # print(df.head(5))
+
+        # print the total number of rows and columns
+        print("#Rows = %s, #Columns = %s" % (len(df),  len(df.columns)))
         for col in df.columns:
             # print column-wise details
             print("About %s ::" % col)
+
             # check whether any missing values are there or not
             print("     #Missing/NaN values = %s" % df[col].isnull().sum())
+
             # check column wise unique value count
             print("     #Unique values = %s" % (len(df[col].unique())))
