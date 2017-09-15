4c3447557c682ef47c89cc686174461e67fd1b88
Sourabh Maity <sourabh.maity@teradata.com>
2017-09-16 00:53:16 +0530

    changes.

--- AVclickPred.py
+++ AVclickPred.py
@@ -7,3 +7,6 @@ df = raw_data
 
 # let's explore the data
 DE.explore_data(raw_data)
+
+# let's see the memory consumption
+print("Data consumed %s gb." % (df.memory_usage(index=True, deep=True).sum() / (1024 * 1024)))
